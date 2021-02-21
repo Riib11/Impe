9ec4d1cce37ea5a2fc130398f54610971ac12a6b
Henry Blanchette <henry@monkeyface.com>
2021-02-20 22:29:25 -0500

    simple test of typing and executing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,26 @@
+import Language.Impe.Executing
 import Language.Impe.Grammar
 import Language.Impe.Typing
 
+display :: Show a => String -> a -> IO ()
+display tag value = do
+  putStrLn . unlines $
+    [ "[" ++ tag ++ "]",
+      "",
+      "  " ++ show value,
+      ""
+    ]
+
 main :: IO ()
-main =
+main = do
+  putStrLn "[typing]"
   case runTyping (synthesizeInstruction inst) of
-    Left err -> putStrLn "[error]\n" >> putStrLn err
-    Right (t, ctx) -> putStrLn "[success]\n" >> putStrLn ("  " ++ show t)
+    Left err -> display "error" err
+    Right (t, ctx) -> display "success" t
+  putStrLn "[executing]"
+  case runExecuting (executeInstruction inst) of
+    Left err -> display "error" err
+    Right (e, ctx) -> display "success" e
   where
     inst =
       Block
