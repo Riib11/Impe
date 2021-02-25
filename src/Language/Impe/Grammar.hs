2918fc0c3a787a06a7c8ed2313f37d0cba6af1e9
Henry Blanchette <henry@monkeyface.com>
2021-02-24 19:25:04 -0500

    grammar show instances

--- src/Language/Impe/Grammar.hs
+++ src/Language/Impe/Grammar.hs
@@ -1,8 +1,11 @@
 module Language.Impe.Grammar where
 
+import Data.List (intercalate)
+import Text.Printf
+
 data Program
   = Program [Instruction]
-  deriving (Show, Eq)
+  deriving (Eq)
 
 data Instruction
   = Block [Instruction]
@@ -14,7 +17,7 @@ data Instruction
   | Return Expression
   | ProcedureCall Name [Expression] -- procedures are functions that don't return a value.
   | PrimitiveFunctionBody Name [Name] -- placeholder for primitive function bodies.
-  deriving (Show, Eq)
+  deriving (Eq)
 
 data Type
   = VoidType
@@ -22,7 +25,7 @@ data Type
   | IntType
   | BoolType
   | FunctionType [Type] Type
-  deriving (Show, Eq)
+  deriving (Eq)
 
 data Expression
   = Unit
@@ -30,7 +33,7 @@ data Expression
   | Int Int
   | Reference Name
   | Application Name [Expression]
-  deriving (Show, Eq)
+  deriving (Eq)
 
 newtype Name
   = Name String
@@ -43,7 +46,51 @@ mainName = Name "main"
 ## Show instances
 -}
 
--- TODO
+instance Show Program where
+  show = \case
+    Program insts -> intercalate " " . (show <$>) $ insts
+
+instance Show Instruction where
+  show = \case
+    Block insts ->
+      intercalate " " . (show <$>) $ insts
+    Declaration x t ->
+      printf "%s: %s;" (show x) (show t)
+    Assignment x e ->
+      printf "%s <- %s;" (show x) (show e)
+    Function x params t inst ->
+      printf
+        "%s(%s): %s = %s"
+        (show x)
+        (intercalate ", " . ((\(y, s) -> printf "%s: %s" (show y) (show s)) <$>) $ params)
+        (show t)
+        (show inst)
+    Conditional e inst1 inst2 ->
+      printf "if %s then %s else %s" (show e) (show inst1) (show inst2)
+    Loop e inst ->
+      printf "while %s do %s" (show e) (show inst)
+    Return e ->
+      printf "return %s;" (show e)
+    ProcedureCall f args ->
+      printf "%s(%s);" (show f) (intercalate ", " . (show <$>) $ args)
+    PrimitiveFunctionBody f args ->
+      printf "{{%s(%s)}}" (show f) (intercalate ", " . (show <$>) $ args)
+
+instance Show Type where
+  show = \case
+    VoidType -> "void"
+    UnitType -> "unit"
+    IntType -> "int"
+    BoolType -> "bool"
+    FunctionType ss t -> printf "(%s) -> %s" (intercalate ", " . (show <$>) $ ss) (show t)
+
+instance Show Expression where
+  show = \case
+    Unit -> "unit"
+    Bool b -> show b
+    Int i -> show i
+    Reference x -> show x
+    Application f args -> printf "%s(%s)" (show f) (intercalate ", " . (show <$>) $ args)
 
 instance Show Name where
   show (Name x) = x
