fbf44cee4fb5ed1cca1d28ed61ff582d88d3b206
Henry Blanchette <henry@monkeyface.com>
2021-03-11 17:01:26 -0500

    fixed some fndmntl problems in Namespace impl

--- src/Data/Namespace.hs
+++ src/Data/Namespace.hs
@@ -59,71 +59,63 @@ type instance Index (Namespace n a) = n
 type instance IxValue (Namespace n a) = a
 
 instance Ord n => Ixed (Namespace n a) where
+  -- f :: a -> f a
   ix n f nsp =
-    case lookup n nsp of
-      Just a -> f a <&> \a' -> insert n a' nsp
-      Nothing -> pure nsp
+    case lookupWithUID n nsp of
+      Just (a, uid) -> f a <&> \a' -> nsp & store %~ replaceStore uid a'
+      Nothing -> nsp & pure
   {-# INLINE ix #-}
 
 instance Ord n => At (Namespace n a) where
-  at n f nsp =
-    f (lookup n nsp) <&> \r -> case r of
-      Nothing -> maybe nsp (const (delete n nsp)) (lookup n nsp)
-      Just a' -> insert n a' nsp
+  at n f nsp = case lookupScope n (nsp ^. scope) of
+    Just uid ->
+      f (lookup n nsp) <&> \case
+        Just a' -> nsp & store %~ replaceStore uid a'
+        Nothing -> nsp & delete n
+    Nothing ->
+      f (lookup n nsp) <&> \case
+        Just a' -> nsp & initialize n a'
+        Nothing -> nsp
 
 {-
 ## Interface
+
+analytic:
+- initialize
+- delete
+- lookup
+- alter -- (Maybe a -> Maybe a)
+
+synthetic:
+- update -- (a -> Maybe a)
+- adapt -- (Maybe a -> a)
+- adjust -- (a -> a)
+- replace -- a
+
 -}
 
-newUID :: n -> Namespace n a -> (Namespace n a, UID n)
-newUID n nsp = (nsp & counter %~ succ, (n, nsp ^. counter))
+-- initialize
 
 initialize :: Ord n => n -> a -> Namespace n a -> Namespace n a
 initialize n a nsp =
   let (nsp', uid) = newUID n nsp
    in nsp'
-        & (scope %~ insertIntoScope n uid)
-          . (store %~ insertIntoStore uid a)
+        & (scope %~ initializeScope n uid)
+          . (store %~ initializeStore uid a)
 
--- lookup
+initializeScope :: Ord n => n -> UID n -> Scope n -> Scope n
+initializeScope n uid (m :| scp) = Map.insert n uid m :| scp
 
-lookup :: Ord n => n -> Namespace n a -> Maybe a
-lookup n nsp = do
-  uid <- lookupFromScope n (nsp ^. scope)
-  Map.lookup uid (nsp ^. store)
+initializeStore :: Ord n => UID n -> a -> Store n a -> Store n a
+initializeStore uid a str = Map.insert uid a str
 
-lookupFromScope :: Ord n => n -> Scope n -> Maybe (UID n)
-lookupFromScope n scp = foldl (<|>) Nothing . fmap (Map.lookup n) $ scp
-
-lookupFromStore :: Ord n => UID n -> Store n a -> Maybe a
-lookupFromStore uid str = Map.lookup uid str
-
--- insert
-
-insert :: Ord n => n -> a -> Namespace n a -> Namespace n a
-insert n a nsp =
-  case lookupFromScope n (nsp ^. scope) of
-    Just uid ->
-      nsp
-        & (scope %~ insertIntoScope n uid)
-          . (store %~ insertIntoStore uid a)
-    Nothing ->
-      -- let (nsp', uid) = newUID n nsp
-      --  in nsp'
-      --       & (scope %~ insertIntoScope n uid)
-      --         . (store %~ insertIntoStore uid a)
-      nsp
-
-insertIntoScope :: Ord n => n -> UID n -> Scope n -> Scope n
-insertIntoScope n uid (m :| scp) = Map.insert n uid m :| scp
-
-insertIntoStore :: Ord n => UID n -> a -> Store n a -> Store n a
-insertIntoStore uid a str = Map.insert uid a str
+newUID :: n -> Namespace n a -> (Namespace n a, UID n)
+newUID n nsp = (nsp & counter %~ succ, (n, nsp ^. counter))
 
 -- delete
 
 delete :: Ord n => n -> Namespace n a -> Namespace n a
-delete n nsp = case lookupFromScope n (nsp ^. scope) of
+delete n nsp = case lookupScope n (nsp ^. scope) of
   Just uid ->
     nsp
       & (foldl (.) id)
@@ -145,6 +137,67 @@ deleteFromScope n scp = go scp
 deleteUID :: Ord n => UID n -> Namespace n a -> Namespace n a
 deleteUID uid nsp = nsp & store %~ Map.delete uid
 
+deleteUIDs :: Ord n => [UID n] -> Namespace n a -> Namespace n a
+deleteUIDs uids nsp = foldl (flip deleteUID) nsp uids
+
+-- lookup
+
+lookup :: Ord n => n -> Namespace n a -> Maybe a
+lookup n nsp = do
+  uid <- lookupScope n (nsp ^. scope)
+  Map.lookup uid (nsp ^. store)
+
+lookupUID :: Ord n => n -> Namespace n a -> Maybe (UID n)
+lookupUID n nsp = lookupScope n (nsp ^. scope)
+
+lookupWithUID :: Ord n => n -> Namespace n a -> Maybe (a, UID n)
+lookupWithUID n nsp = do
+  a <- lookup n nsp
+  uid <- lookupUID n nsp
+  return (a, uid)
+
+lookupScope :: Ord n => n -> Scope n -> Maybe (UID n)
+lookupScope n scp = foldl (<|>) Nothing . fmap (Map.lookup n) $ scp
+
+lookupStore :: Ord n => UID n -> Store n a -> Maybe a
+lookupStore uid str = Map.lookup uid str
+
+-- alter
+
+alter :: Ord n => n -> (Maybe a -> Maybe a) -> Namespace n a -> Namespace n a
+alter n f nsp = case lookupScope n (nsp ^. scope) of
+  Just uid -> case f (lookup n nsp) of
+    Just a' -> nsp & store %~ replaceStore uid a'
+    Nothing -> nsp & delete n
+  Nothing ->
+    case f (lookup n nsp) of
+      Just a' -> nsp & initialize n a'
+      Nothing -> nsp
+
+-- update, adapt, adjust, replace
+
+update :: Ord n => n -> (a -> Maybe a) -> Namespace n a -> Namespace n a
+update n f = alter n (>>= f)
+
+adapt :: Ord n => n -> (Maybe a -> a) -> Namespace n a -> Namespace n a
+adapt n f = alter n (Just . f)
+
+adjust :: Ord n => n -> (a -> a) -> Namespace n a -> Namespace n a
+adjust n f = alter n (f <$>)
+
+replace :: Ord n => n -> a -> Namespace n a -> Namespace n a
+replace n a = alter n (\_ -> Just a)
+
+adaptStore :: Ord n => UID n -> (a -> Maybe a) -> Store n a -> Store n a
+adaptStore uid f str = case lookupStore uid str of
+  Just a -> case f a of
+    Just a' -> str & Map.adjust (\_ -> a') uid
+    Nothing -> str & Map.delete uid
+  Nothing -> str
+
+replaceStore :: Ord n => UID n -> a -> Store n a -> Store n a
+replaceStore uid a = adaptStore uid (\_ -> Just a)
+
 {-
 ## Utilities
 -}
@@ -155,10 +208,10 @@ enterScope nsp = nsp & scope %~ (mempty <|)
 leaveScope :: Ord n => Namespace n a -> Namespace n a
 leaveScope nsp = case nsp ^. scope of
   _ :| [] -> nsp & scope .~ mempty :| mempty
-  _ :| (m : scp) -> nsp & scope .~ m :| scp
-
--- _ :| [] -> nsp & garbagecollect . (scope .~ mempty :| mempty)
--- _ :| (m : scp) -> nsp & garbagecollect . (scope .~ m :| scp)
+  m :| (m' : scp) ->
+    nsp
+      & deleteUIDs (Map.elems m)
+        . (scope .~ m' :| scp)
 
 isVisible :: Ord n => n -> Namespace n a -> Bool
 isVisible n nsp = isJust $ lookup n nsp
